96205f54eb3d9391b3cecd9b8df4ff1c3e648ff8
Rando-Lurkz <johnnyb.1998@ymail.com>
2026-01-01 15:44:33 -0800

    fixed mistake of not adding main.py to commit

--- main.py
+++ main.py
@@ -1,15 +1,5 @@
-def get_book_text(file_path):
-    with open(file_path) as f:
-        files_contents = f.read()
-    return files_contents
-
-def get_word_count(string):
-    words_list = string.split()
-    word_count = len(words_list)
-    return word_count
-
+from stats import get_char_count
 def main():
-    word_count = get_word_count(get_book_text("books/frankenstein.txt"))
-    print(f"Found {word_count} total words")
+    print(get_char_count("books/frankenstein.txt"))
 
 main()
