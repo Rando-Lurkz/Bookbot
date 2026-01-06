869470ec39bd9195c4b87ff051e6ce838fbc2f67
Rando-Lurkz <johnnyb.1998@ymail.com>
2026-01-05 22:33:50 -0800

    Finished integrating argv

--- main.py
+++ main.py
@@ -1,5 +1,18 @@
-from stats import get_char_count
+import sys
+from stats import get_char_count, sort_char_count, get_word_count
+
+print("Usage: python3 main.py <path_to_book>")
+
 def main():
-    print(get_char_count("books/frankenstein.txt"))
+    num_words = get_word_count("books/frankenstein.txt")
+    print("============ BOOKBOT ============")
+    print("Analyzing book found at books/frankenstein.txt...")
+    print("----------- Word Count ----------")
+    print(f"Found {num_words} total words")
+    
+    list_of_char_count = sort_char_count(get_char_count(sys.argv[1]))
+    for item in list_of_char_count:
+        if item["char"].isalpha():
+            print(f"{item["char"]}: {item["num"]}")
 
 main()
--- stats.py
+++ stats.py
@@ -7,11 +7,20 @@ def get_word_count(file_path):
 
 def get_char_count(file_path):
     with open(file_path) as f:
-        book_text = f.read().lstrip("\ufeff")
+        book_text = f.read()
     char_count = {}
     for letter in book_text:
         l = letter.lower()
         if l not in char_count:
-            char_count[l] = 0
-        char_count[l] += 1
+            char_count[l] = {"char": l, "num": 0}
+        char_count[l]["num"] += 1
     return char_count
+
+def sort_char_count(char_count):
+    to_sort = []
+    for item in char_count:
+        to_sort.append(char_count[item])
+    def sort_by(char_dict):
+        return char_dict["num"]
+    to_sort.sort(reverse=True, key=sort_by)
+    return to_sort
